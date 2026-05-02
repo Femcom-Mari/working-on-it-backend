59a95b5d723152fec2b476d1dc13f7eceb3b306e
Femcom-Mari <bongollmaria@gmail.com>
2026-05-02 17:20:09 +0200

    initial commit

---
+++ src/main/java/com/workingionit/WorkingOnItApplication.java
@@ -0,0 +1,13 @@
+package com.workingionit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingOnItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingOnItApplication.class, args);
+	}
+
+}
